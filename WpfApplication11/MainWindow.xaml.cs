ac63a4ec1a5d47e11c4ec450124348c4eb94eec0
Edson Pachao Garcia <e.pachao.garcia@isur.edu.pe>
2013-11-28 07:29:51 -0500

    AGENDA
    AUN SE PERCIBEN PROBLEMAS PARA UBICAR EL PAD DE EL ARCHIVO SQL  POR LO
    DEMAS SOLO QUEDARIA ARREGLAR EL INTERFACE GRAFICO

---
+++ WpfApplication11/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WpfApplication11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button5_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection cnx = new SqlConnection(@"server=.;database=AGENDA;Integrated Security =True");
+            //SqlCommand cmd = new SqlCommand("REPORTE", cnx);
+            SqlCommand cmd = new SqlCommand("REPORTE", cnx);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            SqlDataAdapter DA = new SqlDataAdapter(cmd);
+            //DataSet DS = new DataSet();
+            DataTable DT = new DataTable();
+            cnx.Open();
+            //cmd.ExecuteNonQuery();
+            DA.Fill(DT);
+            dataGrid1.ItemsSource = DT.DefaultView;
+            cnx.Close();
+            //MessageBox.Show("hey");
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void listBox1_Loaded(object sender, RoutedEventArgs e)
+        {
+            mostrar_usuarios();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 ventana = new Window2();
+            ventana.Owner = this;
+            ventana.ShowDialog();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            mostrar_usuarios();
+        }
+        void mostrar_usuarios()
+        {
+            System.Data.SqlClient.SqlConnection cnx = new System.Data.SqlClient.SqlConnection(@"server=.;database=AGENDA;Integrated Security =True");
+            cnx.Open();
+            System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand("select nombre from dbo.PERSONAS", cnx);
+            System.Data.SqlClient.SqlDataReader DR = cmd.ExecuteReader();
+            //DR = cmd.ExecuteReader();
+            listBox1.Items.Clear();
+            while (DR.Read())
+            {
+                listBox1.Items.Add(DR[0].ToString());
+            }
+            DR.Close();
+            cnx.Close();
+        }
+
+        private void listBox1_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            SqlConnection cnx = new SqlConnection(@"server=.;database=AGENDA;Integrated Security =True");
+            //SqlCommand cmd = new SqlCommand("REPORTE", cnx);
+            SqlCommand cmd = new SqlCommand("informacion", cnx);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.Add("@NOMBRE", System.Data.SqlDbType.Text, 50);
+            cmd.Parameters["@NOMBRE"].Value = Convert.ToString(listBox1.SelectedItem);
+            //MessageBox.Show(listBox1.AreAnyTouchesCaptured);
+            SqlDataAdapter DA = new SqlDataAdapter(cmd);
+            //DataSet DS = new DataSet();
+            DataTable DT = new DataTable();
+            cnx.Open();
+            //cmd.ExecuteNonQuery();
+            DA.Fill(DT);
+            dataGrid1.ItemsSource = DT.DefaultView;
+            cnx.Close();
+            //MessageBox.Show("hey");
+        }
+    }
+}
---
+++ WpfApplication11/Window2.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace WpfApplication11
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection cnx = new SqlConnection("server=.; database=AGENDA; Integrated Security=true");
+            SqlCommand cmd = new SqlCommand("NUEVO_PERSONA", cnx);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.Add("@NOMBRE", System.Data.SqlDbType.Text, 50);
+            cmd.Parameters.Add("@APELLIDO", System.Data.SqlDbType.Text, 50);
+            cmd.Parameters.Add("@CORREO_ELECTRONICO", System.Data.SqlDbType.Text, 100);
+            cmd.Parameters.Add("@FECHA_DE_NACIMIENTO", System.Data.SqlDbType.Date);
+            cmd.Parameters.Add("@DNI", System.Data.SqlDbType.Text, 8);
+                //@NOMBRE, @APELLIDO, @CORREO_ELECTRONICO, @FECHA_DE_NACIMIENTO, @DNI
+            cmd.Parameters["@NOMBRE"].Value = textBox5.Text;
+            cmd.Parameters["@APELLIDO"].Value = textBox4.Text;
+            cmd.Parameters["@CORREO_ELECTRONICO"].Value = textBox3.Text;
+            cmd.Parameters["@FECHA_DE_NACIMIENTO"].Value = Convert.ToDateTime(textBox2.Text);
+            cmd.Parameters["@DNI"].Value = textBox1.Text;
+            cnx.Open();
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("INGRESO CORRECTO");
+            cnx.Close();
+            ventana1.Close();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            ventana1.Close();
+        }
+    }
+}
